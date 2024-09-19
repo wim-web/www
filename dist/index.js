005d7f211ddfaf9b489df4d90e7dfa879bb3177a
wim-web <45655999+wim-web@users.noreply.github.com>
2024-09-20 02:24:17 +0900

    init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const hello = () => {
+    console.log("hello new world");
+};
+exports.hello = hello;
